0cda545b46674fa3851d737e45fdf782020205cb
soe-j <soeda@val.co.jp>
2014-11-25 17:08:58 +0900

    typeリストとそれに対応したskillリストの説明（と、どうでもいい変更）

--- type.rb
+++ type.rb
@@ -1,8 +1,11 @@
 class Type
   include ActiveModel::Model
+
   attr_accessor :name
+
+  # typeとskillの対応表
   type_list = %w(ノーマル ほのお みず でんき くさ こおり)
-  skill_list = %w(ぱんち ほのおのぱんち みずでっぽう 10万ボルト はっぱカッター アイス)
+  skill_list = %w(ぱんち ほのおのぱんち みずでっぽう 10万ボルト はっぱカッター アイスノン)
   @@type_skill_list = {}
   type_list.each_with_index do |type,i|
     @@type_skill_list["#{type}"] = skill_list[i]
